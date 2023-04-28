823ef8f17da5b524122fc7cc79c6bfd3f66884cf
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-04-28 22:40:31 +0530

    api setup

---
+++ src/utils/api.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+// global method to fetch the data
+
+const BASE_URL="https://api.themoviedb.org/3";
+const TMDB_TOKEN=import.meta.env.VITE_APP_TMDB_TOKEN; // syntax is for vite env variables access
+
+// refer to https://apipheny.io/api-headers/ to know more about api headers
+const headers={
+      Authorization: "bearer "+TMDB_TOKEN,
+}
+
+const fetchData=async(url,params)=>{
+  try{
+     const {data}=await axios.get(BASE_URL+url,{
+      headers,  
+      params,
+     })
+     console.log(data);
+     return data; 
+  }
+  catch(error){
+      console.log(error);
+      return error;
+  }
+}
+export default fetchData;

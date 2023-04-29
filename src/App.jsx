212c7aad61aa869f5b9b9d9b025892ce68c6d2cf
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-04-29 09:08:47 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import { useEffect } from 'react'
+// import { useEffect } from 'react'
 import './App.css'
-import fetchData from './utils/api'
-
+import {Routes,Route ,BrowserRouter as Router} from 'react-router-dom'
+// import fetchData from './utils/api'
+// import { useDispatch,useSelector} from 'react-redux'
+// import { getURL } from './store/slices/homePage';
+import Navbar from './components/Navbar'
+import Error from './pages/404/Error'
+import Details from './pages/details/Details'
+import Explore from './pages/explore/Explore'
+import HomePage from './pages/homepage/HomePage'
+import Searchresult from './pages/searchresult/Searchresult'
+import Footer from './components/Footer'
 function App() {
-  useEffect(()=>{
-     fetchData("/movie/popular");
-  },[])
   return (
     <>
-     Hello
+     <Router>
+     <Navbar/>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/:showtype/:id' element={<Details/>}/>
+        <Route path='search/:input' element={<Searchresult/>}/>
+        <Route path='*' element={<Error/>}/>
+        <Route path='explore/:showtype' element={<Explore/>}/>
+      </Routes>
+      <Footer/>
+     </Router>
     </>
   )
 }

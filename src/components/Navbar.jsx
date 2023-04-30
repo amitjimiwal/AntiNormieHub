abb6d6503440d948cc4f52dbc474923c70c85538
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-04-30 16:31:36 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,72 @@
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
+// import iconimg from '../../public/movix-logo.png'
 const Navbar = () => {
+  const [isMenuOpen,setisMenuOpen]=useState(false);
+  const handleToggleMenu=()=>{
+    setisMenuOpen(!isMenuOpen);
+  }
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="sticky w-full z-[100] bg-black">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link to="/" className="flex items-center">
+          {/* <img
+            src={iconimg}
+            className="h-8 mr-3"
+            alt="Flowbite Logo"
+          /> */}
+          <span className="self-center xd:text-xl font-semibold whitespace-nowrap dark:text-white xsm:text-[1rem]">
+            AntiNormieHub
+          </span>
+        </Link>
+        <button
+          data-collapse-toggle="navbar-hamburger"
+          type="button"
+          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-hamburger"
+          aria-expanded="false"
+          onClick={handleToggleMenu}
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-6 h-6"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        <div className={isMenuOpen ?"absolute top-[57px] w-full sticky top-[75px]":"hidden w-full"} id="navbar-hamburger">
+          <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+            <li>
+              <a
+                href="#"
+                // className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded dark:bg-blue-600"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                aria-current="page"
+              >
+                Movies
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                Tv Shows
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;

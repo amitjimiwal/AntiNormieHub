00cb714ed5116198a18305e059a49f9bc3ced7f9
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-05-01 23:03:13 +0530

    footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="bg-black text-center w-full p-2 text-white">
+      Made with Sarcasm ❤️ by <a href="https://github.com/amitjimiwal" className="text-green-400" target="_blank" rel="noreferrer">Amit Jimiwal</a>
+    </div>
   )
 }
 

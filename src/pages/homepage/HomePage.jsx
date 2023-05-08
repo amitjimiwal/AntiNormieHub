90a98576bc4ee6bbc5ae6938b18e7a08ea51959e
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-05-09 00:09:03 +0530

    component name fix

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,12 +1,11 @@
-import useFetch from "../../hooks/useFetch"
 import Banner from "./sections/Banner"
-import Section from "./sections/Trending"
+import Trending from "./sections/Trending"
 
 const HomePage = () => {
   return (
     <div className="w-full">
       <Banner/>
-      <Section name='trending'/>
+      <Trending name='trending'/>
     </div>
   )
 }

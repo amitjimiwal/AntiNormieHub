9f0d83ffda34c56360e3fa31e8941a68a95933c2
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-05-28 01:14:37 +0530

    minor changes in styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <div className="bg-black text-center w-full p-2 text-white sticky bottom-0">
+    <div className="bg-black text-center w-full p-2 text-white sticky bottom-0 z-20">
       Made with Sarcasm ❤️ by <a href="https://github.com/amitjimiwal" className="text-green-400" target="_blank" rel="noreferrer">Amit Jimiwal</a>
     </div>
   )

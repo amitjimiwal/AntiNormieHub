fca25f3201e3e6372fc60a1b3edc29f653a34491
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-06-24 18:08:26 +0530

    feat : pwa build added

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,17 @@ import App from './App.jsx'
 import './index.css'
 import { store } from './store/store.js'
 import { Provider } from 'react-redux'
+
+import { registerSW } from "virtual:pwa-register";
+
+// add this to prompt for a refresh
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      updateSW(true);
+    }
+  },
+});
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
